1fbf18f80158a1a3342f6d232defd061e615aa9a
Tom Mertens <tommertensjobappliation@gmail.com>
2025-06-21 16:53:29 +0200

    fix: Update base URL in api_root function from 127.0.0.1 to localhost

--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'http://127.0.0.1:8000/'
+    base_url = 'http://localhost:8000/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
